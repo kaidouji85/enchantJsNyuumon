931193efdfdde6fe4fcb0d3ba877a8209af6865f
kaidouji85 <kaidouji85@gmail.com>
2014-06-21 13:13:29 +0900

    キャラクター表示

--- javascripts/game.js
+++ javascripts/game.js
@@ -3,14 +3,29 @@ enchant();
 var core;
 var GAME_WIDTH = 320;
 var GAME_HEIGHT = 320;
+var PLAYER_WIDTH = 64;
+var PLAYER_HEIGHT = 64;
+var PLAYER_PICT = "images/jiki.png";
+var playerSprite;
 
 window.onload = function() {
-    core = new Core(GAME_WIDTH, GAME_HEIGHT);
-    core.fps = 60;
-    core.rootScene.backgroundColor = "black";
-    core.onload = function() {
+    initCore();
+    preLoad();
 
+    core.onload = function() {
+        playerSprite = new Sprite(PLAYER_WIDTH,PLAYER_HEIGHT);
+        playerSprite.image = core.assets[PLAYER_PICT];
+        core.rootScene.addChild(playerSprite);
     };
     core.start();
 };
 
+function initCore(){
+    core = new Core(GAME_WIDTH, GAME_HEIGHT);
+    core.fps = 60;
+    core.rootScene.backgroundColor = "black";
+}
+
+function preLoad(){
+    core.preload(PLAYER_PICT);
+}
