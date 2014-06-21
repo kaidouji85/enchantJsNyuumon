1f55205ea49cbe86a4f3e3c1426bb98229a7a292
kaidouji85 <kaidouji85@gmail.com>
2014-06-21 13:28:44 +0900

    プレイヤーが移動できるようになった。

--- javascripts/game.js
+++ javascripts/game.js
@@ -6,6 +6,7 @@ var GAME_HEIGHT = 320;
 var PLAYER_WIDTH = 64;
 var PLAYER_HEIGHT = 64;
 var PLAYER_PICT = "images/jiki.png";
+var PLAYER_SPEED = 4;
 var playerSprite;
 
 window.onload = function() {
@@ -13,8 +14,7 @@ window.onload = function() {
     preLoad();
 
     core.onload = function() {
-        playerSprite = new Sprite(PLAYER_WIDTH,PLAYER_HEIGHT);
-        playerSprite.image = core.assets[PLAYER_PICT];
+        playerSprite = definePlayerSprite();
         core.rootScene.addChild(playerSprite);
     };
     core.start();
@@ -28,4 +28,38 @@ function initCore(){
 
 function preLoad(){
     core.preload(PLAYER_PICT);
+}
+
+function definePlayerSprite(){
+    var player = new Sprite(PLAYER_WIDTH,PLAYER_HEIGHT);
+    player.image = core.assets[PLAYER_PICT];
+    player.addEventListener(Event.ENTER_FRAME,function(e){
+        //移動
+        if(core.input.up){
+            player.y -= PLAYER_SPEED;
+        } else if(core.input.down){
+            player.y += PLAYER_SPEED;
+        }
+        if(core.input.right){
+            player.x += PLAYER_SPEED;
+        } else if(core.input.left) {
+            player.x -= PLAYER_SPEED;
+        }
+
+        //画面外移動の防止
+        if(player.x<0){
+            player.x = 0;
+        } else if(player.x > GAME_WIDTH-PLAYER_WIDTH){
+            player.x = GAME_WIDTH-PLAYER_WIDTH;
+        }
+        if(player.y<0){
+            player.y = 0;
+        } else if(player.y > GAME_HEIGHT-PLAYER_HEIGHT){
+            player.y = GAME_HEIGHT-PLAYER_HEIGHT;
+        }
+
+    });
+    player.x = GAME_WIDTH/2;
+    player.y = GAME_HEIGHT/2;
+    return player;
 }
